37bf8ea3001f2e951ed67dfc120ea9f9286b4304
Benjamin Gee <benjamin.n.gee@gmail.com>
2024-04-25 17:03:10 -0600

    Excercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,6 +3,15 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
+import { BLOG_TITLE } from '@/constants';
+
+export const generateMetadata = async ({ params }) => {
+    const { frontmatter } = await loadBlogPost(params.postSlug);
+    return {
+        title: `${frontmatter.title} • ${BLOG_TITLE}`,
+        description: frontmatter.abstract
+    };
+};
 
 async function BlogPost({ params }) {
     const { frontmatter, content } = await loadBlogPost(params.postSlug);
