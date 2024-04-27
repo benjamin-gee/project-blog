71ffb1840a7794c862c71c84f0da30d82c8f5ab9
Benjamin Gee <benjamin.n.gee@gmail.com>
2024-04-26 20:33:22 -0600

    Excercise 6: Dark mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,59 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
 import { Rss, Sun, Moon } from 'react-feather';
+import Cookie from 'js-cookie';
 
 import Logo from '@/components/Logo';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './Header.module.css';
+import { COLOR_THEME_COOKIE, DARK_TOKENS, LIGHT_TOKENS } from '@/constants';
 
-function Header({ theme, className, ...delegated }) {
-  return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
-      <Logo />
+function Header({ initialTheme, className, ...delegated }) {
+    const [theme, setTheme] = React.useState(initialTheme);
 
-      <div className={styles.actions}>
-        <button className={styles.action}>
-          <Rss
-            size="1.5rem"
-            style={{
-              // Optical alignment
-              transform: 'translate(2px, -2px)',
-            }}
-          />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
-        </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
-        </button>
-      </div>
-    </header>
-  );
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        console.log(newTheme);
+        setTheme(newTheme);
+        Cookie.set(COLOR_THEME_COOKIE, newTheme, { expires: 1000 });
+        const newTokens = newTheme === 'light' ? LIGHT_TOKENS : DARK_TOKENS;
+
+        const root = document.documentElement;
+
+        root.setAttribute('data-color-theme', newTheme);
+        Object.entries(newTokens).forEach(([key, value]) => {
+            root.style.setProperty(key, value);
+        });
+    };
+
+    return (
+        <header className={clsx(styles.wrapper, className)} {...delegated}>
+            <Logo />
+
+            <div className={styles.actions}>
+                <button className={styles.action}>
+                    <Rss
+                        size='1.5rem'
+                        style={{
+                            // Optical alignment
+                            transform: 'translate(2px, -2px)'
+                        }}
+                    />
+                    <VisuallyHidden>View RSS feed</VisuallyHidden>
+                </button>
+                <button className={styles.action} onClick={toggleTheme}>
+                    {theme === 'light' ? (
+                        <Sun size='1.5rem' />
+                    ) : (
+                        <Moon size='1.5rem' />
+                    )}
+                    <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
+                </button>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
